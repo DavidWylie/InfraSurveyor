db73856a9e5bc550786752eb60804eced095eb73
DavidWyli <david.leo.wylie@googlemail.com>
2022-04-19 14:10:30 +0100

    Add icons loader tests

--- tests/test_icons.py
+++ tests/test_icons.py
@@ -1,8 +1,39 @@
 import unittest
+from surveyor import icons
+from surveyor.cloud import models
+from surveyor_aws_icons.loader import _get_resource_dir
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
+class TestIcon(unittest.TestCase):
+    def test_load_empty_nodes(self):
+        nodes = []
+        expected_icons = []
+        icons.load_icons(nodes)
+        self.assertEqual(expected_icons, nodes)
+
+    def test_load_existing_icon(self):
+        nodes = [
+            models.Resource(
+                id="test-id",
+                name="test",
+                category="ANALYTICS",
+                service="AWS-Glue",
+                resource_type="Crawler",
+            )
+        ]
+        expected_nodes = [
+            models.Resource(
+                id="test-id",
+                name="test",
+                category="ANALYTICS",
+                service="AWS-Glue",
+                resource_type="Crawler",
+                image=f"{_get_resource_dir()}/Resource-Icons_01312022/Res_Analytics/Res_48_Light/Res_AWS-Glue_Crawler_48_Light.svg",
+            )
+        ]
+        icons.load_icons(nodes)
+        self.assertEqual(expected_nodes, nodes)
+
+
+if __name__ == "__main__":
     unittest.main()
