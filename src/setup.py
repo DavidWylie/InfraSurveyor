ff99e3123aa2e5163cb3d0ea2f1a68ec9aea318e
DavidWyli <david.leo.wylie@googlemail.com>
2022-04-19 13:49:46 +0100

    Add black to file wwatchers in ide and actions

--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='surveyor',
-    version='0.1',
+    name="surveyor",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
-        'boto3',
-        'boto3-stubs',
-        'graphviz',
+        "click",
+        "boto3",
+        "boto3-stubs",
+        "graphviz",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         surveyor=surveyor.commands:cli
-    ''',
+    """,
 )
