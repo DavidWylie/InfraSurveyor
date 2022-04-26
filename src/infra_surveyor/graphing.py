43f4658a23a6a2f72388a196f916a9a118da9619
DavidWyli <david.leo.wylie@googlemail.com>
2022-04-26 11:52:52 +0100

    :bug: hash ids in dot file to avoid naming conflicts and incompatible ids.

--- src/infra_surveyor/graphing.py
+++ src/infra_surveyor/graphing.py
@@ -1,6 +1,7 @@
 import graphviz
 from infra_surveyor.cloud import models
 from typing import List
+import hashlib
 
 
 class Graph:
@@ -27,14 +28,20 @@ class Graph:
             },
         )
 
+    def _hash_id(self, id: str) -> str:
+        return hashlib.sha256(id.encode()).hexdigest()
+
     def assemble_graph(self, nodes: List[models.Resource], links: List[models.Link]):
         for node in nodes:
-            self.graph.node(node.id, node.name, image=node.image)
+            safe_id = self._hash_id(node.id)
+            self.graph.node(safe_id, node.name, image=node.image)
 
         for link in links:
+            safe_source = self._hash_id(link.source)
+            safe_dest = self._hash_id(link.destination)
             self.graph.edge(
-                f"{link.source}:e",
-                f"{link.destination}:w",
+                f"{safe_source}:e",
+                f"{safe_dest}:w",
             )
 
     def render_graph(self, file_name, directory, output_format):
